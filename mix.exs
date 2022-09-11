05e43209b7261fa4422e56f22b1a9bfbf389fab8
Emre Akinci <eemreakinci@gmail.com>
2022-09-11 19:12:31 +0300

    Format file mix.exs

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule PaymentConfirmation.Umbrella.MixProject do
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
     [
-      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
     ]
   end
 
