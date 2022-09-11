ea70808a65f3e51269a23a3b4f8c1d4cd14ab49c
Emre Akinci <eemreakinci@gmail.com>
2022-09-11 18:07:48 +0300

    Setup dialyxir

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule PaymentConfirmation.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
-    []
+    [
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
